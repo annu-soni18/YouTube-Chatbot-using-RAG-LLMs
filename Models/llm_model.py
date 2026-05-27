6da7b83d7ac7d21d449fb38cd2c77768480473c9
annu-soni-18 <annusoni7797@gmail.com>
2026-05-27 12:22:42 +0530

    Added API model

--- Models/llm_model.py
+++ Models/llm_model.py
@@ -4,6 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 load_dotenv()
 
@@ -23,8 +24,8 @@ def load_llm():
 @st.cache_resource
 def load_embeddings():
 
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-small"
     )
 
     return embeddings
